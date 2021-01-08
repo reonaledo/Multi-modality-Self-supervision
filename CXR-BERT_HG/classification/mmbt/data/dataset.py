c3a3be215b4002bab99293ed6aa83a24cd919a65
Hyungyung Lee <ttumyche@gmail.com>
2021-01-08 23:35:38 +0900

    Downstream task: Classification

---
+++ CXR-BERT_HG/classification/mmbt/data/dataset.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python3
+#
+# Copyright (c) Facebook, Inc. and its affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+#
+
+import json
+import numpy as np
+import os
+from PIL import Image
+
+import torch
+from torch.utils.data import Dataset
+
+from utils.utils import truncate_seq_pair, numpy_seed
+
+
+class JsonlDataset(Dataset):
+    def __init__(self, data_path, tokenizer, transforms, vocab, args):
+        self.data = [json.loads(l) for l in open(data_path)]
+        self.data_dir = os.path.dirname(data_path)
+        self.tokenizer = tokenizer
+        self.args = args
+        self.vocab = vocab
+        self.n_classes = len(args.labels)
+        self.text_start_token = ["[CLS]"] if args.model != "mmbt" else ["[SEP]"]
+
+        with numpy_seed(0):
+            for row in self.data:
+                if np.random.random() < args.drop_img_percent:
+                    row["img"] = None
+
+        self.max_seq_len = args.max_seq_len
+        if args.model == "mmbt":
+            self.max_seq_len -= args.num_image_embeds
+
+        self.transforms = transforms
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, index):
+        sentence = (
+            self.text_start_token
+            + self.tokenizer(self.data[index]["text"])[
+                : (self.args.max_seq_len - 1)
+            ]
+        )
+        segment = torch.zeros(len(sentence))
+
+        sentence = torch.LongTensor(
+            [
+                self.vocab.stoi[w] if w in self.vocab.stoi else self.vocab.stoi["[UNK]"]
+                for w in sentence
+            ]
+        )
+        if self.args.task_type == "multilabel":
+            label = torch.zeros(self.n_classes)
+            if self.data[index]["label"] == '':
+                self.data[index]["label"] = "'Others'"
+            else:
+                pass  
+            label[
+                [self.args.labels.index(tgt) for tgt in self.data[index]["label"].split(', ')]
+            ] = 1
+        else:
+            input("이거는 multilabel 학습 아니니 다시 돌리세요~")
+            pass
+
+        image = None
+        if self.args.model in ["img", "concatbow", "concatbert", "mmbt"]:
+            if self.data[index]["img"]:
+                image = Image.open(
+                    os.path.join(self.data_dir, self.data[index]["img"])
+                ).convert("RGB")
+            else:
+                image = Image.fromarray(128 * np.ones((256, 256, 3), dtype=np.uint8))
+            image = self.transforms(image)
+
+        if self.args.model == "mmbt":
+            # The first SEP is part of Image Token.
+            segment = segment[1:]
+            sentence = sentence[1:]
+            # The first segment (0) is of images.
+            segment += 1
+
+        return sentence, segment, image, label
